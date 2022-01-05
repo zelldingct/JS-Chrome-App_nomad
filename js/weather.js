2dc33c4eb2e05eb70b938702ccc98b39540b7c91
zelldingct <zell9931@gmail.com>
2022-01-05 18:54:07 +0900

    Making App Done

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+const API_KEY = "e839a9f893b5ac4ba40190397107c8bf";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const location = document.querySelector("#weather span:last-child");
+      location.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
